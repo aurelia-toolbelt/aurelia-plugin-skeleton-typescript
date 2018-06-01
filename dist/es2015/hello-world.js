c71eff1b7abcd64c8c29f2774905616a6a32c5fe
Saeed Ganji <shahabganji@gmail.com>
2018-06-01 12:50:19 +0430

    update(gitignore file)

--- dist/es2015/hello-world.js
+++
@@ -1,8 +0,0 @@
-var HelloWorld = (function () {
-    function HelloWorld() {
-        this.msg = 'Hello Aurelia!';
-    }
-    return HelloWorld;
-}());
-export { HelloWorld };
-//# sourceMappingURL=hello-world.js.map
--- dist/es2015/index.js
+++
@@ -1,5 +0,0 @@
-export * from './hello-world';
-export function configure(config) {
-    config.globalResources('./hello-world');
-}
-//# sourceMappingURL=index.js.map
